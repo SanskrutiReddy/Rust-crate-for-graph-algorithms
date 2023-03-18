b3abfc7d4525567a7188d3c00cd13cd7e3e47c5f
pavanbodanki <pavanbodanki@gmail.com>
2023-03-18 13:37:52 -0400

    returning edges as vector
    vector getting returned.Edges are ready and need to perform the algorithm

--- graphs/src/list_of_algorithms/bellmanford.rs
+++ graphs/src/list_of_algorithms/bellmanford.rs
@@ -23,14 +23,17 @@ pub mod bellmanford {
         stdin().read_line(&mut n_edges).expect("Enter Valid Input");
         let n_edges: i32 = n_edges.trim().parse().expect("Invalid input for source");
         //assign the weights to each edge from the console
-        let mut edges = Vec![add_weights(vertices, source, n_edges)];
+        let mut edges = add_weights(vertices, source, n_edges);
         print!("If you want add edges press 1")
         // add new edges if the user want to add more edges at this point
         //implementation of bellman ford algorithm
     }
-    pub fn add_weights(vertices:String,source:String,edges:String)-> &Vec
-    {
-        for i in 0..(edges-1)
+
+
+
+    pub fn add_weights(vertices: usize, source: usize, edges: i32) -> Vec<(usize, usize, i32)>
+    {   let mut weighted_edges = vec![];
+        for i in 0..(edges)
         {   
             let mut s = String::new();
             let mut d = String::new();
@@ -47,7 +50,9 @@ pub mod bellmanford {
             let _= stdout().flush();
             stdin().read_line(&mut w).expect("Please Enter Valid Input for .");
             let w: i32 = w.trim().parse().expect("Invalid input for source");
+            weighted_edges.push((s, d, w));
          }
+        return weighted_edges;
          //return vector in the form containing source,destination and weight of the edge
     }
 }    
